0fd89902dd74e29ad2f713832767825c3ae257bd
kiranRaveendran <kiranraveendran50@gmail.com>
2024-10-28 20:30:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("theme-toggle").addEventListener("change", function () {
+    // Toggle dark-mode/light-mode class on body
+    document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("light-mode");
+  });
+  
